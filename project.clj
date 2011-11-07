db83676552c26600af41e665ea38b45f4dfe87de
R. Mattes <rm@seid-online.de>
2011-11-07 21:51:21 +0100

    Initial git setup

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject de.tuxteam.cms.notify "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [postgresql "9.1-901.jdbc4"]
+                ;; [postgresql/postgresql "8.4-702.jdbc4"]
+                ;; [org.clojure/java.jdbc "0.1.1"]
+                 [org.clojars.amit/smack "3.1.0"]
+                 [org.clojars.amit/smackx "3.1.0"]]
+
+  :main de.tuxteam.cms.notify.core)
---
+++ test/de/tuxteam/cms/notify/test/core.clj
@@ -0,0 +1,6 @@
+(ns de.tuxteam.cms.notify.test.core
+  (:use [de.tuxteam.cms.notify.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
